2a9e737d8d20d46e284840cfe400dd3431dab599
Islam <islamderrouiche@gmail.com>
2024-11-14 23:09:34 +0100

    Finalisation du javascript

--- 1- Frontend/Script/index.js
+++ 1- Frontend/Script/index.js
@@ -1,44 +1,71 @@
-    // FORMULAIRE D'AVIS SUR LA PAGE D'ACCUEIL
+//-------------------HABITAT PAGE D'ACCEUIL---------------------
 
-    document.querySelector(`.avis`).onsubmit = function() {
+// document.addEventListener('DOMContentLoaded', function() {
+// function bigImg(x) {
+//     x.style.width = "548px";
+//     x.style.height = "375px";
+
+// };
+
+// function normalImg(x) {
+//     x.style.width = "448px";
+//     x.style.height = "275px";
+
+// };
+//    document.querySelector(`.habitat-img`).onmouseenter = function() {
+//     onmouseover = bigImg(this);  
+//    };
+
+//    document.querySelector(`.habitat-img`).onmouseleave = function() {
+//     onmouseout = normalImg(this);
+//    };
+
+// });
+
+//-------------FORMULAIRE D'AVIS SUR LA PAGE D'ACCUEIL------------
+
+    document.addEventListener('DOMContentLoaded', function() {
+       document.querySelector(`.avis`).onsubmit = function() {
         const name = document.querySelector('#pseudo').value;
         alert(`Votre Commentaire a bien été envoyé au nom de ${name}. Merci pour votre avis`);
     };
+});
+
+   
 
 
-    // FORMULAIRE D'AVIS SUR LA PAGE DE CONTACT
+//-------------FORMULAIRE D'AVIS SUR LA PAGE DE CONTACT--------
 
-    document.querySelector(`#contactForm`).onsubmit = function() {
-        alert(`Le formulaire a bien été envoyé.`);
-    };
-
-
-
-
+    document.addEventListener('DOMContentLoaded', function() {
+        document.querySelector(`#contactForm`).onsubmit = function() {
+            alert("Le formulaire a bien été envoyé.");
+        };
+});  
 
 
-// function hello() {
-//     let button =  document.getElementById("button-avis")
-//     let pseudo = document.getElementById("pseudo");
-//     let comment = document.getElementById("comment");
-//     if (button.onclick && pseudo != "" && comment != "") {
-//          alert ('Hello, world');
-//     } else{
-//         alert ("remplissez le formulaireavant d'envoyer");
-//     }
-//  }
+//------------------DATE COPYRIGHT FOOTER-----------------
 
-/*function validateForm() {
-    let x = document.forms["avisForm"]["avisButton"].value;
-    if (x === true) {
-        alert("Votre Commentaire a bien été envoyé. Merci pour votre avis");
-    }
-}*/
-// document.querySelector("button-avis").onclick = hello();
+    document.addEventListener('DOMContentLoaded', function() {
+        new Date().getFullYear();
+        document.getElementById("year").innerHTML = new Date().getFullYear();
+});  
 
 
 
-//DATE COPYRIGHT FOOTE
+//------------------ANIMAUX PAGE D'ACCUEIL-----------------
 
-new Date().getFullYear();
-document.getElementById("year").innerHTML = new Date().getFullYear();
+function enter(x) {
+    x.style.boxShadow = "15px 15px 10px 0px rgb(110, 134, 0)";
+};
+
+function leave(x) {
+    x.style.boxShadow = "0px 0px 0px 0px rgb(110, 134, 0)";
+};
+
+   document.querySelector(`.container-animaux`).onmouseenter = function() {
+    onmouseover = enter(this);    
+   };
+
+   document.querySelector(`.container-animaux`).onmouseleave = function() {
+    onmouseout = leave(this);
+   };
