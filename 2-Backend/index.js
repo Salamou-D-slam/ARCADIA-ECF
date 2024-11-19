98f1e36eadf37780ab76ee34d09b83a999ec4bf7
Islam <islamderrouiche@gmail.com>
2024-11-19 16:54:05 +0100

    formulaire de connexion avec la DB

--- 2-Backend/index.js
+++ 2-Backend/index.js
@@ -3,40 +3,93 @@ import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
+import pg from "pg";
 
 
 const app = express();
 const port = 3000;
 
-var userIsAuthorised = false;
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "Arcadia DB",
+  password: "setifsalamou_19000",
+  port: 5432,
+});
+
+
+
+db.connect();
+
+// let compte = [];
+// db.query("SELECT * FROM utilisateurs", (err, res) => {
+//   if (err) {
+//     console.error("Error executing query", err.stack);
+//   } else {
+//     compte = res.rows;
+//   }
+//   db.end();
+// });
+
+
+// var userIsAuthorised = false;
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 
-function passGenerator(req ,res, next) {
-    const email = req.body["email"];
-    const password = req.body["password"]; //regarder le name dans le formulair html
-    if(email === "admin@gmail.com" && password === "admin19000"){
-        userIsAuthorised = true;
-    } else {
-        userIsAuthorised = false;
-      }
-    next ();
-}
+// function passGenerator(req ,res, next) {
+//     const email = req.body["email"];
+//     const password = req.body["password"]; //regarder le name dans le formulair html
+//     if(email === "admin@gmail.com" && password === "admin19000"){
+//         userIsAuthorised = true;
+//     } else {
+//         userIsAuthorised = false;
+//       }
+//     next ();
+// }
 
-app.use(passGenerator);
+// app.use(passGenerator);
  
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/connexion_bk.html");
 });
 
-app.post("/check", (req, res) => {
-    if (userIsAuthorised) {
+app.post("/check", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+
+  try {
+    const result = await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+
+    if (result.rows.length > 0) {
+      const user = result.rows[0];
+      const storedPassword = user.password;
+
+      if (password === storedPassword) {
         res.sendFile(__dirname + "/public/secret.html");
       } else {
-        res.sendFile(__dirname + "/public/connexion_bk.html");
-        //Alternatively res.redirect("/");
+        res.send("Mot de passe incorrect.");
       }
+      
+    } else {
+      res.send("utilisateur non trouvé.");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+
+
+
+
+    // if (userIsAuthorised) {
+    //     res.sendFile(__dirname + "/public/secret.html");
+    //   } else {
+    //     res.sendFile(__dirname + "/public/connexion_bk.html");
+    //     //Alternatively res.redirect("/");
+    //   }
 });
 
 app.listen(port, () => {
---
+++ 2-Backend/queries.sql
@@ -0,0 +1,54 @@
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR (100),
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR (100)
+);
+
+CREATE table roles (
+  id SERIAL primary key,
+  label varchar (100)
+);
+
+
+CREATE table services(
+  id SERIAL primary key,
+  nom varchar (100),
+  description varchar(100)
+);
+
+CREATE table animal(
+  id SERIAL primary key,
+  prenom varchar (100),
+  etat varchar(100)
+);
+
+CREATE table rapport_veterinaire(
+  id SERIAL primary key,
+  date date (100),
+  detail varchar(100)
+);
+
+CREATE table habitat(
+  id SERIAL primary key,
+  nom varchar (100),
+  description varchar(100),
+  commentaire_habitat varchar (100)
+);
+
+CREATE table image(
+  id SERIAL primary key,
+  image_data blob
+);
+
+CREATE table race(
+  id SERIAL primary key,
+  abel varchar (100)
+);
+
+CREATE table avis(
+  pseudo SERIAL primary key,
+  nom varchar (100),
+  commentaire varchar(100),
+  isVisible Bool
+);
